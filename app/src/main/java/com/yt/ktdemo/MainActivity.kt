982fc37568e49ebeae7a4fe747641b9465b7f405
yangtian <864805692@qq.com>
2019-07-16 19:27:10 +0800

    test1

--- app/src/main/java/com/yt/ktdemo/MainActivity.kt
+++ app/src/main/java/com/yt/ktdemo/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.yt.ktdemo
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews();
 
+    }
+
+    private fun initViews() {
+        var btn_scanner = findViewById<Button>(R.id.main_btn_scanner)
     }
 }
